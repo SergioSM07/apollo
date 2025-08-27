bc68360cea751b687db045850d78d732de7896a9
SergioSM07 <checho070623@hotmail.com>
2025-08-26 23:44:20 -0500

    Update environment.ts

--- frontend/front-apollo/src/environments/environment.ts
+++ frontend/front-apollo/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAC7-__cPwmdNOns-4pzx89nPZkq6psRqs",
-    authDomain: "kata-apollo.firebaseapp.com",
-    projectId: "kata-apollo",
-    storageBucket: "kata-apollo.firebasestorage.app",
-    messagingSenderId: "223770138765",
-    appId: "1:223770138765:web:2435aa3d9d363a008baac4",
-    measurementId: "G-WNLBM5E443"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSAGE_SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "MEASUREMENT_ID"
   }
 };
